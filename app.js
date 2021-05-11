41149d56c464c65ab19ebd883d01fdcb761a49e0
zarriei <75182441+zarriei@users.noreply.github.com>
2021-05-11 00:38:05 -0400

    pages resolved

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
+import NavTabs from "./components/pages/NavTabs";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Portfolio from "./components/pages/Portfolio";
