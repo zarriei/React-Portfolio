61862892b28915e75eb7d9acf801e1888c70cc9e
zarriei <75182441+zarriei@users.noreply.github.com>
2021-05-11 19:49:45 -0400

    added text to about me and contact

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # React-Portfolio
+
+#Description
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -14,8 +14,24 @@ function Home() {
         consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et
         malesuada fames ac ante ipsum primis in faucibus.
       </p>
+      
+      < className="row">
+        <div className="col-md-12 text-info">
+          <section className="second section text-white">
+            <h2>Inspirational Quotes</h2>
+            <p className="text-white border border-danger">Quote comes from Denzel Washington and his commencement speech at Howard University
+              <q>Fail big, dream big, and dreams without goals are only dreams</q>
+            </p>
+            <p className="text-dark bg-white border border-danger">Another quote from Denzel Washington <q>Just because your doing a lot more doesn't
+                mean
+                your getting a lot more done</q></p>
+            <p className="bg-dark text-white">This is a quote from Michele Ruiz <q>If people are doubting how far you can
+                go,
+                go so far that you can't hear them anymore</q></p>
+          </section>
     </div>
+  </div>
   );
-}
+};
 
 export default Home;
